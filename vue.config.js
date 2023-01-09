5479089e4dc3ed3716975d6a80ca7d5f07bd0c5d
MartinShelley <martin.shelley91@gmail.com>
2023-01-09 13:37:08 +0000

    deploying

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/Audiophile-Ecommerce-Site',
   transpileDependencies: true
 })
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/Audiophile-Ecommerce-Site/'
-//     : '/'
-// }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Audiophile-Ecommerce-Site/'
+    : '/'
+}
